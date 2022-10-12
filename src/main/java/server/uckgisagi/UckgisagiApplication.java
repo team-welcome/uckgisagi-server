f65c362291ea386ac99ebf8d3879aa4d663e448f
chanwoo <chocw0402@gmail.com>
2022-10-12 20:46:53 +0900

    initial commit

---
+++ src/main/java/server/uckgisagi/UckgisagiApplication.java
@@ -0,0 +1,13 @@
+package server.uckgisagi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UckgisagiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UckgisagiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/server/uckgisagi/UckgisagiApplicationTests.java
@@ -0,0 +1,13 @@
+package server.uckgisagi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UckgisagiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
