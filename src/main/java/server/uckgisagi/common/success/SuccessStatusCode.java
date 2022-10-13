bed6cec55ae8a1bc8aaf573975c2671e411d50a2
chanwoo <chocw0402@gmail.com>
2022-10-14 01:15:55 +0900

    feat : Enum success status code

---
+++ src/main/java/server/uckgisagi/common/success/SuccessStatusCode.java
@@ -0,0 +1,19 @@
+package server.uckgisagi.common.success;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum SuccessStatusCode {
+
+    OK(200),
+    CREATED(201),
+    ACCEPTED(202),
+    NO_CONTENT(204)
+    ;
+
+    private final int status;
+
+}
