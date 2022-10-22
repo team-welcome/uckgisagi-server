855f5584e861f9ed3ab389fd7ea22be78b16495a
chanwoo <chocw0402@gmail.com>
2022-10-23 03:09:39 +0900

    feat : post repostiroy 개발

---
+++ src/main/java/server/uckgisagi/domain/post/repository/PostRepository.java
@@ -0,0 +1,7 @@
+package server.uckgisagi.domain.post.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import server.uckgisagi.domain.post.entity.Post;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
