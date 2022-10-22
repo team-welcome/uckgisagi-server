5ef585b0040f0cdb1da26a6ecf90a2e03dd52b98
chanwoo <chocw0402@gmail.com>
2022-10-23 02:59:47 +0900

    feat : user repostiroy 개발

---
+++ src/main/java/server/uckgisagi/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package server.uckgisagi.domain.user.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import server.uckgisagi.domain.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
