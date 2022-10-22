7d696160ca90dba3939e3655d5193c2449970d1a
chanwoo <chocw0402@gmail.com>
2022-10-23 03:11:59 +0900

    feat : 클라이언트 요청 보호 config

---
+++ src/main/java/server/uckgisagi/config/security/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package server.uckgisagi.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class WebSecurityConfig {
+
+    /**
+     * 인터셉터로 요청을 안전하게 보호하는 방법을 설정
+     * @param http
+     * @throws Exception
+     */
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                    .anyRequest().permitAll() // 토큰을 활용하는 경우 모든 요청에 대해 접근 가능하도록 설정
+                .and()
+                    .httpBasic().and()
+                    .formLogin().disable() // form 기반 로그인 비활성화
+                    .logout().disable()
+                    .csrf().disable()
+                    // session 사용 안함
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .headers()
+                    .frameOptions().sameOrigin()
+                    .cacheControl().and()
+                    .xssProtection().and()
+                    .httpStrictTransportSecurity().disable();
+        return http.build();
+    }
+
+}
