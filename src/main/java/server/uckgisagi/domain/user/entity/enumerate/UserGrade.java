e557d879d531bb926f72cb7f36138d0d5d761861
chanwoo <chocw0402@gmail.com>
2022-10-23 02:58:58 +0900

    feat : enum 회원등급

---
+++ src/main/java/server/uckgisagi/domain/user/entity/enumerate/UserGrade.java
@@ -0,0 +1,37 @@
+package server.uckgisagi.domain.user.entity.enumerate;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import server.uckgisagi.common.model.EnumModel;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum UserGrade implements EnumModel {
+
+    SQUIRE("종자", 2),
+    BARON("남작", 5),
+    EARL("백작", 10),
+    DUKE("공작", 19),
+    LORD("영주", 32),
+    KING("왕", 53),
+    ;
+
+    private final String value;
+    private final int accumulate;
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    public int getAccumulate() {
+        return accumulate;
+    }
+
+}
