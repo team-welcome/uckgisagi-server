6cd073d4553858ec3d903e0d7ab24942d9d5071e
chanwoo <chocw0402@gmail.com>
2022-10-23 03:07:40 +0900

    feat : enum tag 이름

---
+++ src/main/java/server/uckgisagi/domain/store/entity/enumerate/TagName.java
@@ -0,0 +1,30 @@
+package server.uckgisagi.domain.store.entity.enumerate;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import server.uckgisagi.common.model.EnumModel;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum TagName implements EnumModel {
+
+    GROCERY("식료품"),
+    DETERGENT("세제"),
+    BEAUTY("미용"),
+    ETC("기타"),
+    ;
+
+    private final String value;
+
+    @Override
+    public String getKey() {
+        return name();
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+}
