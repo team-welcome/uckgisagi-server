09ea7c1e63c07dd1bab749817aa1af301ec889d8
chanwoo <chocw0402@gmail.com>
2022-10-24 00:02:04 +0900

    feat : 리필스테이션(store) 요약 정보 서비스 내 response dto

---
+++ src/main/java/server/uckgisagi/app/store/dto/response/PreviewStoreDto.java
@@ -0,0 +1,33 @@
+package server.uckgisagi.app.store.dto.response;
+
+import lombok.*;
+import server.uckgisagi.domain.store.entity.Store;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class PreviewStoreDto {
+
+    private Long storeId;
+    private String storeName;
+    private String address;
+    private String imageUrl;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    private PreviewStoreDto(final Long storeId, final String storeName, final String address, final String imageUrl) {
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.address = address;
+        this.imageUrl = imageUrl;
+    }
+
+    public static PreviewStoreDto from(Store store) {
+        return PreviewStoreDto.builder()
+                .storeId(store.getId())
+                .storeName(store.getName())
+                .address(store.getAddress())
+                .imageUrl(store.getImageUrl())
+                .build();
+    }
+
+}
