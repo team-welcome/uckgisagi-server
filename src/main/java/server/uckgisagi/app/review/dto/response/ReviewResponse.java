eed2213d61fe42a824d3ba8568d28342736d23e1
chanwoo <chocw0402@gmail.com>
2022-10-24 15:31:35 +0900

    feat : review response 개발

---
+++ src/main/java/server/uckgisagi/app/review/dto/response/ReviewResponse.java
@@ -0,0 +1,30 @@
+package server.uckgisagi.app.review.dto.response;
+
+import lombok.*;
+import server.uckgisagi.domain.review.entity.Review;
+
+@ToString
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ReviewResponse {
+
+    private Long reviewId;
+    private String nickname;
+    private String comment;
+
+    @Builder(access = AccessLevel.PACKAGE)
+    private ReviewResponse(final Long reviewId, final String nickname, final String comment) {
+        this.reviewId = reviewId;
+        this.nickname = nickname;
+        this.comment = comment;
+    }
+
+    public static ReviewResponse from(Review review) {
+        return ReviewResponse.builder()
+                .reviewId(review.getId())
+                .nickname(review.getUser().getNickname())
+                .comment(review.getComment())
+                .build();
+    }
+
+}
