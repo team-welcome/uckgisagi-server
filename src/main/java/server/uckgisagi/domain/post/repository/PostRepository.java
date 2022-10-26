78ab976ac0d9fe2b9f3763f5f85be6af13501643
chanwoo <chocw0402@gmail.com>
2022-10-26 17:20:49 +0900

    add : PostRepositoryCustom 확장

--- src/main/java/server/uckgisagi/domain/post/repository/PostRepository.java
+++ src/main/java/server/uckgisagi/domain/post/repository/PostRepository.java
@@ -3,5 +3,5 @@ package server.uckgisagi.domain.post.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.uckgisagi.domain.post.entity.Post;
 
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 }
