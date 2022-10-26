0ac132c8e61f264323ac666ebd7ac37ace327a94
chanwoo <chocw0402@gmail.com>
2022-10-26 17:21:45 +0900

    feat : PostRepositoryCustomImpl 구현 개발

---
+++ src/main/java/server/uckgisagi/domain/post/repository/PostRepositoryCustomImpl.java
@@ -0,0 +1,57 @@
+package server.uckgisagi.domain.post.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import server.uckgisagi.domain.post.entity.Post;
+import server.uckgisagi.domain.user.entity.User;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static server.uckgisagi.domain.post.entity.QPost.*;
+
+@RequiredArgsConstructor
+public class PostRepositoryCustomImpl implements PostRepositoryCustom {
+
+    private final JPAQueryFactory query;
+
+    private static final long ONE_DAY = 1L;
+
+    @Override
+    public List<Post> findPostByUserId(Long userId) {
+        return query
+                .selectFrom(post)
+                .where(post.user.id.eq(userId))
+                .fetch();
+    }
+
+    @Override
+    public boolean existsByTodayDate(LocalDate today, Long userId) {
+        return query
+                .selectOne()
+                .from(post)
+                .where(
+                        post.user.id.eq(userId),
+                        post.createdAt.between(
+                                today.atStartOfDay(),
+                                today.atStartOfDay().plusDays(ONE_DAY)
+                        ))
+                .fetchFirst() != null;
+    }
+
+
+    @Override
+    public List<User> findUserIdsByTodayDate(LocalDate today, List<Long> userIds) {
+        return query
+                .select(post.user)
+                .from(post)
+                .where(
+                        post.user.id.in(userIds),
+                        post.createdAt.between(
+                                today.atStartOfDay(),
+                                today.atStartOfDay().plusDays(ONE_DAY)
+                        ))
+                .fetch();
+    }
+
+}
