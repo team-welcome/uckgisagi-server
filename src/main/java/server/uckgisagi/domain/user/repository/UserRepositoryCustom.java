27e289fe0ce53a25d80e835cd1720aaa5802321a
chanwoo <chocw0402@gmail.com>
2022-10-28 18:09:32 +0900

    add : 닉네임으로 모든 유저 찾기 메서드 추가

--- src/main/java/server/uckgisagi/domain/user/repository/UserRepositoryCustom.java
+++ src/main/java/server/uckgisagi/domain/user/repository/UserRepositoryCustom.java
@@ -2,6 +2,9 @@ package server.uckgisagi.domain.user.repository;
 
 import server.uckgisagi.domain.user.entity.User;
 
+import java.util.List;
+
 public interface UserRepositoryCustom {
     User findUserByUserId(Long userId);
+    List<User> findAllUserByNickname(String nickname);
 }
