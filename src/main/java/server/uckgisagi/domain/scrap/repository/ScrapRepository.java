285af70684bd466091598189bc5d2f98a4ce079b
chanwoo <chocw0402@gmail.com>
2022-10-30 00:29:39 +0900

    rename : entity 명 scrap 으로 변경에 따른 repository명 변경

--- src/main/java/server/uckgisagi/domain/scrap/repository/ScrapRepository.java
+++ src/main/java/server/uckgisagi/domain/scrap/repository/ScrapRepository.java
@@ -3,5 +3,5 @@ package server.uckgisagi.domain.scrap.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.uckgisagi.domain.scrap.entity.Scrap;
 
-public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+public interface ScrapRepository extends JpaRepository<Scrap, Long>, ScrapRepositoryCustom {
 }
