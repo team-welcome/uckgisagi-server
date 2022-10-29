e5f15e04ce1d872acac8452ed6f16aea41af0cd3
chanwoo <chocw0402@gmail.com>
2022-10-29 14:35:04 +0900

    refactor : 두번째 파라미터 User 타입으로 변경

--- src/main/java/server/uckgisagi/app/notification/service/NotificationService.java
+++ src/main/java/server/uckgisagi/app/notification/service/NotificationService.java
@@ -1,5 +1,7 @@
 package server.uckgisagi.app.notification.service;
 
+import server.uckgisagi.domain.user.entity.User;
+
 public interface NotificationService {
-    void sendNotification(Long userId, Long friendUserId);
+    void sendNotification(Long userId, User friendUserId);
 }
