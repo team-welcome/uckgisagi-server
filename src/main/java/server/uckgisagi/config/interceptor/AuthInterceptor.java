6d3bef972019fc4a3fa27a58ecdd39a3f2f6b28b
chanwoo <chocw0402@gmail.com>
2022-11-03 11:25:42 +0900

    feat : HandlerInterceptor 조합 서블릿 요청을 가로채 Auth 여부 판단

---
+++ src/main/java/server/uckgisagi/config/interceptor/AuthInterceptor.java
@@ -0,0 +1,34 @@
+package server.uckgisagi.config.interceptor;
+
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.servlet.HandlerInterceptor;
+import server.uckgisagi.config.security.JwtConstants;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+    private final LoginCheckHandler loginCheckHandler;
+
+    @Override
+    public boolean preHandle(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull Object handler) throws Exception {
+        if (!(handler instanceof HandlerMethod)) {
+            return true;
+        }
+        HandlerMethod handlerMethod = (HandlerMethod) handler;
+        Auth auth = handlerMethod.getMethodAnnotation(Auth.class);
+        if (auth == null) {
+            return true;
+        }
+        Long userId = loginCheckHandler.getUserId(request);
+        request.setAttribute(JwtConstants.USER_ID, userId);
+        return true;
+    }
+
+}
