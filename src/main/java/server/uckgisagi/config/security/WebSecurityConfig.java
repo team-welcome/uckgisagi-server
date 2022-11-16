3b255bf7c6a742bc08deeac27c47682d489c2448
chanwoo <chocw0402@gmail.com>
2022-11-17 00:32:27 +0900

    add : @EnableWebSecurity 추가

--- src/main/java/server/uckgisagi/config/security/WebSecurityConfig.java
+++ src/main/java/server/uckgisagi/config/security/WebSecurityConfig.java
@@ -3,10 +3,12 @@ package server.uckgisagi.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class WebSecurityConfig {
 
     /**
@@ -34,5 +36,4 @@ public class WebSecurityConfig {
                     .httpStrictTransportSecurity().disable();
         return http.build();
     }
-
 }
