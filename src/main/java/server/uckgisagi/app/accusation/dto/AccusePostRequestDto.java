fadb800e4c8d53ed3a27366c402b95748eb42134
Dayz-new <sdhdream99@naver.com>
2022-11-20 17:03:33 +0900

    add : Accuse 관련 Dto 클래스 생성

---
+++ src/main/java/server/uckgisagi/app/accusation/dto/AccusePostRequestDto.java
@@ -0,0 +1,15 @@
+package server.uckgisagi.app.accusation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AccusePostRequestDto {
+
+    private Long postId;
+}
