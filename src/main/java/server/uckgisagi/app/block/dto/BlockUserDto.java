be200203e798b884c48ce0c4e663eb4add325642
Dayz-new <sdhdream99@naver.com>
2022-11-30 17:52:06 +0900

    feat : 유저 차단하기 BlockUserDto 추가

---
+++ src/main/java/server/uckgisagi/app/block/dto/BlockUserDto.java
@@ -0,0 +1,16 @@
+package server.uckgisagi.app.block.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BlockUserDto {
+
+    private Long blockUserId;
+}
+
