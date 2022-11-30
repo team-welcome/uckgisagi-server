3f3c7a9981b7173d65cc3c8ce39e2602dabd4332
chanwoo <chocw0402@gmail.com>
2022-11-30 14:50:25 +0900

    test : ApplicationContext 에 등록된 빈 테스트

--- src/test/java/server/uckgisagi/app/ApplicationContextBeanTest.java
+++ src/test/java/server/uckgisagi/app/ApplicationContextBeanTest.java
@@ -1,4 +1,4 @@
-package server.uckgisagi;
+package server.uckgisagi.app;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -11,14 +11,14 @@ import org.springframework.web.servlet.HandlerMapping;
 
 import java.util.Map;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.config.location=classpath:application-test.yml"})
 public class ApplicationContextBeanTest {
 
     @Autowired
     private ApplicationContext applicationContext;
 
-    @Test
     @DisplayName("등록된 모든 Bean 조회")
+    @Test
     void retrieve_all_beans() {
         if (applicationContext != null) {
             String[] beans = applicationContext.getBeanDefinitionNames();
@@ -40,10 +40,11 @@ public class ApplicationContextBeanTest {
      *<br>
      * order: 2147483646 resourceHandlerMapping = SimpleUrlHandlerMapping
      */
-    @Test
     @DisplayName("Spring 에서 자동으로 추가하는 HnadlerMapping의 우선순위 조회")
+    @Test
     void retrieve_HandlerMapping() {
-        Map<String, HandlerMapping> matchingBeans = BeanFactoryUtils.beansOfTypeIncludingAncestors(applicationContext, HandlerMapping.class, true, false);
+        Map<String, HandlerMapping> matchingBeans = BeanFactoryUtils
+                .beansOfTypeIncludingAncestors(applicationContext, HandlerMapping.class, true, false);
         matchingBeans.forEach((k, v) -> {
             System.out.printf(
                     "order: %s %s = %s %n%n",
