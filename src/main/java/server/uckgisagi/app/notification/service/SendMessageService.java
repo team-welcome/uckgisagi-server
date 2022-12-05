56404f88ba8f45e229618310266b7f0d19b0534b
chanwoo <chocw0402@gmail.com>
2022-12-05 15:42:46 +0900

    fix : FCM 알림이 가지 않는 오류 수정

--- src/main/java/server/uckgisagi/app/notification/service/SendMessageService.java
+++ src/main/java/server/uckgisagi/app/notification/service/SendMessageService.java
@@ -1,65 +1,118 @@
 package server.uckgisagi.app.notification.service;
 
-import com.google.api.core.ApiFuture;
-import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.api.core.ApiFutureCallback;
+import com.google.api.core.ApiFutures;
+import com.google.common.util.concurrent.MoreExecutors;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
-import server.uckgisagi.app.notification.domain.entity.Notification;
+import server.uckgisagi.app.notification.domain.entity.Notifications;
 import server.uckgisagi.app.notification.domain.entity.enumerate.NotificationType;
 import server.uckgisagi.app.notification.domain.repository.NotificationRepository;
 import server.uckgisagi.app.user.domain.entity.User;
+import server.uckgisagi.config.firebase.FirebaseInitializer;
+
+import static server.uckgisagi.app.notification.constant.NotificationConstants.*;
 
 public abstract class SendMessageService {
 
-    protected void sendMessageByNotificationType(NotificationRepository notificationRepository, Long userId,
-                                                 User friend, NotificationType type, Logger log) {
+    protected void sendMessageByNotificationType(NotificationRepository notificationRepository, User user, User friend, NotificationType type, Logger log) {
+
+        setCommonFields(notificationRepository, user, friend, log);
         switch (type) {
             case POKE:
-                sendAsyncFCM(
-                        notificationRepository,
-                        getPokeMessage(userId, friend.getUserFcmToken()),
-                        userId,
-                        friend.getId(),
-                        log);
+                setPokeFields();
                 break;
             case FOLLOW:
-                sendAsyncFCM(
-                        notificationRepository,
-                        getFollowMessage(userId, friend.getUserFcmToken()),
-                        userId,
-                        friend.getId(),
-                        log);
+                setFollowFields();
                 break;
         }
+        sendAsyncFCM();
     }
 
-    private void sendAsyncFCM(NotificationRepository notificationRepository, Message message,
-                              Long userId, Long friendUserId, Logger log) {
-        ApiFuture<String> sendAsync = FirebaseMessaging.getInstance().sendAsync(message);
-
-        if (sendAsync.isDone()) {
-            log.info("✅ 푸시알림 전송 완료 ✅");
-            notificationRepository.save(Notification.newInstance(
-                    userId, friendUserId, NotificationType.POKE, message.toString())
-            );
-        } else if (sendAsync.isCancelled()) {
-            log.info("❌ 푸시알림 전송 실패 ❌");
-        }
+    private void sendAsyncFCM() {
+        ApiFutures.addCallback(
+                FirebaseInitializer.getFirebaseMessaging().sendAsync(message),
+                getApiFutureCallback(),
+                MoreExecutors.directExecutor());
     }
 
-    private Message getPokeMessage(Long userId, String targetUserToken) {
+    @NotNull
+    private ApiFutureCallback<String> getApiFutureCallback() {
+        return new ApiFutureCallback<String>() {
+            @Override
+            public void onFailure(Throwable t) {
+                log.error(FAILURE_MESSAGE);
+            }
+
+            @Override
+            public void onSuccess(String result) {
+                notificationRepository.save(
+                        Notifications.newInstance(userId, friendUserId, notificationType, notificationMessage)
+                );
+                log.info(SUCCESS_MESSAGE);
+            }
+        };
+    }
+
+    private Message getPokeMessage() {
         return Message.builder()
-                .putData("title", "쥰내 찌르기 알림 도착 ㅋㅋ")
-                .putData("content", String.format("%s 님이 너를 쥰내게 찔렀어요 ㅋㅋ", userId))
-                .setToken(targetUserToken)
+                .setToken(targetToken)
+                .setNotification(getPokeNotification())
                 .build();
     }
 
-    private Message getFollowMessage(Long userId, String targetUserToken) {
+    private Notification getPokeNotification() {
+        return new Notification(
+                POKE_TITLE,
+                String.format(POKE_BODY, userNickname)
+        );
+    }
+
+    private Message getFollowMessage() {
         return Message.builder()
-                .putData("title", "쥰내 팔로우 알림 도착 ㅋㅋ")
-                .putData("content", String.format("%s 님이 너를 쥰내게 팔로우했어요 ㅋㅋ", userId))
-                .setToken(targetUserToken)
+                .setToken(targetToken)
+                .setNotification(getFollowNotification())
                 .build();
+    }
+
+    private Notification getFollowNotification() {
+        return new Notification(
+                String.format(FOLLOW_TITLE, userNickname),
+                String.format(FOLLOW_BODY, userNickname)
+        );
+    }
+
+    private NotificationRepository notificationRepository;
+    private Long userId;
+    private String userNickname;
+    private Long friendUserId;
+    private String targetToken;
+    private Logger log;
+
+    private NotificationType notificationType;
+    private Message message;
+    private String notificationMessage;
+
+    private void setFollowFields() {
+        this.notificationType = NotificationType.FOLLOW;
+        this.message = getFollowMessage();
+        this.notificationMessage = String.format(FOLLOW_MESSAGE, userNickname);
+    }
+
+    private void setPokeFields() {
+        this.notificationType = NotificationType.POKE;
+        this.message = getPokeMessage();
+        this.notificationMessage = String.format(POKE_MESSAGE, userNickname);
+    }
+
+    private void setCommonFields(NotificationRepository notificationRepository, User user, User friend, Logger log) {
+        this.notificationRepository = notificationRepository;
+        this.userId = user.getId();
+        this.userNickname = user.getNickname();
+        this.friendUserId = friend.getId();
+        this.targetToken = friend.getUserFcmToken();
+        this.log = log;
     }
 }
